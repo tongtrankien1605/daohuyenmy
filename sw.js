4e111f0765d6de805846bd55dc14fd34f48ad6fa
tongtrankien1605 <tongtrankien1605@gmail.com>
2025-06-08 00:45:03 +0700

    Update code

--- sw.js
+++ sw.js
@@ -14,6 +14,7 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then(cacheNames => 
             Promise.all(cacheNames.map(cacheName => 
@@ -24,24 +25,17 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    if (event.request.url.includes("/daohuyenmy/music/tran-ngoc-anh.mp4")) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(cache => 
-                cache.match(event.request).then(response => 
-                    response || fetch(event.request, { cache: "no-store" }).then(networkResponse => {
-                        if (networkResponse.ok) cache.put(event.request, networkResponse.clone());
-                        return networkResponse;
-                    })
-                )
-            )
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request).then(response => 
-                response || fetch(event.request)
-            )
-        );
-    }
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) console.log("From cache:", event.request.url);
+            return response || fetch(event.request).then(networkResponse => {
+                if (networkResponse.ok && event.request.url.startsWith("https://tongtrankien1605.github.io/daohuyenmy/")) {
+                    caches.open(CACHE_NAME).then(cache => 
+                        cache.put(event.request, networkResponse.clone())
+                    );
+                }
+                return networkResponse;
+            });
+        })
+    );
 });
-
-const cacheWhitelist = [CACHE_NAME];
