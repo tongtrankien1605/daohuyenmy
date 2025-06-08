01825448c1050000c8f22e133aa3ccffea408c9f
tongtrankien1605 <tongtrankien1605@gmail.com>
2025-06-08 10:13:32 +0700

    Update logic code

--- sw.js
+++ sw.js
@@ -7,10 +7,11 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => Promise.all(cacheNames.map((cacheName) => !cacheWhitelist.includes(cacheName) && caches.delete(cacheName))))
-            .then(() => self.clients.claim())
+        caches.keys().then(cacheNames => 
+            Promise.all(cacheNames.map(cacheName => 
+                !cacheWhitelist.includes(cacheName) && caches.delete(cacheName)
+            ))
+        ).then(() => self.clients.claim())
     );
 });
 
@@ -19,31 +20,29 @@ self.addEventListener("fetch", (event) => {
     const cacheKey = new Request(requestUrl.origin + requestUrl.pathname, {
         method: event.request.method,
         headers: event.request.headers,
-        mode: "cors",
-        cache: "default",
+        mode: 'cors',
+        cache: 'default'
     });
 
     event.respondWith(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.match(cacheKey).then((cachedResponse) => {
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(cacheKey).then(cachedResponse => {
                 if (cachedResponse) {
                     console.log("From cache:", event.request.url);
                     return cachedResponse;
                 }
 
-                return fetch(event.request, { mode: "cors" })
-                    .then((networkResponse) => {
-                        if (networkResponse.ok && (event.request.url.includes("tongtrankien1605.github.io/daohuyenmy") || event.request.url.includes("raw.githubusercontent.com"))) {
-                            console.log("Caching:", event.request.url);
-                            const clonedResponse = networkResponse.clone();
-                            cache.put(cacheKey, clonedResponse);
-                        }
-                        return networkResponse;
-                    })
-                    .catch((err) => {
-                        console.error("Fetch failed:", err);
-                        return caches.match("/offline.html");
-                    });
+                return fetch(event.request, { mode: 'cors' }).then(networkResponse => {
+                    if (networkResponse.ok && (event.request.url.startsWith("https://tongtrankien1605.github.io/daohuyenmy/") || event.request.url.includes("raw.githubusercontent.com"))) {
+                        console.log("Caching:", event.request.url);
+                        const clonedResponse = networkResponse.clone();
+                        cache.put(cacheKey, clonedResponse);
+                    }
+                    return networkResponse;
+                }).catch(err => {
+                    console.error("Fetch failed:", err);
+                    return caches.match('/offline.html');
+                });
             });
         })
     );
