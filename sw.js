d3a72235fb9a35b61ed2ec4bbff9312e7dfda26d
tongtrankien1605 <tongtrankien1605@gmail.com>
2025-06-08 09:51:19 +0700

    Update Code

--- sw.js
+++ sw.js
@@ -21,6 +21,7 @@ self.addEventListener("fetch", (event) => {
         method: event.request.method,
         headers: event.request.headers,
         mode: 'cors',
+        credentials: 'omit',
         cache: 'default'
     });
 
@@ -32,7 +33,7 @@ self.addEventListener("fetch", (event) => {
                     return cachedResponse;
                 }
 
-                return fetch(event.request, { mode: 'cors' }).then(networkResponse => {
+                return fetch(event.request, { mode: 'cors', credentials: 'omit' }).then(networkResponse => {
                     if (networkResponse.ok && (event.request.url.includes("tongtrankien1605.github.io/daohuyenmy") || event.request.url.includes("raw.githubusercontent.com"))) {
                         console.log("Caching:", event.request.url);
                         const clonedResponse = networkResponse.clone();
